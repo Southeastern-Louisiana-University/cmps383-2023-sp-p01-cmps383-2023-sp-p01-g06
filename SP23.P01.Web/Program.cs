d42c79c2798832273579e1bfc3b45862e24cb1e3
ashtoncallender <ashton.callender@selu.edu>
2023-01-20 21:06:43 -0600

    Testing
    Testing

--- SP23.P01.Web/Program.cs
+++ SP23.P01.Web/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System;
+
+
+using SP23.P01.Web.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
